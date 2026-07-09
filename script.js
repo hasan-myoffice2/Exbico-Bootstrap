4d2f1293dbaeda85f64fa29b0918681d24ef624f
Hasan <hasan.myoffice2@gmail.com>
2026-07-09 18:27:35 +0600

    hero section & Team Section Updated

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-var swiper = new Swiper(".heroSwiper", {
-  loop: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
 const videoModal = document.getElementById("videoModal");
 const iframe = document.getElementById("youtubeVideo");
 
